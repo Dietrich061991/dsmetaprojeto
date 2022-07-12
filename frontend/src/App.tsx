eb41e4dc8e87dea761443ba35571b72c9cf5cf4b
Sedami Dietrich <didimontcho@gmail.com>
2022-07-12 00:26:25 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
-
+import Notificationbutton from "./components/Notificationbutton/index."
 function App() {
-  return(
-    <h1>ola</h1>
+  return (
+    <>
+        <h1>ola</h1>
+        <Notificationbutton />
+    </>
   )
 }
 
---
+++ frontend/src/components/Notificationbutton/index..tsx
@@ -0,0 +1,12 @@
+
+import icon from '../../asserts/img/icone-notificacao.svg'
+import './styles.css'
+
+function Notificationbutton() {
+    return(
+      <div className="dsmeta-red-botao">
+          <img src={icon} alt="Notificar" />
+      </div>
+    )
+  }
+  export default Notificationbutton
